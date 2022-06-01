168c641009480ae322b98baae1dc07e5511cedab
Intergalactic <106037203+Tronora@users.noreply.github.com>
2022-06-01 22:38:54 +0200

    Ny sortering

--- script.js
+++ script.js
@@ -1,38 +1,28 @@
 
-document.addEventListener('DOMContentLoaded', () => { 
-document.querySelector('thead').addEventListener('click', ev => {
-  let column = 0;
-  switch (ev.target.innerHTML) {
-    case "#":
-      column = 0
-      break;
-    case "Navn":
-      column = 1
-      break;
-       case "Organ":
-      column = 2
-      break;
-    case "Nasjon":
-      column = 3
-      break;
-      case "Funksjon/omtale":
-      column = 4
-      break;
-    case " ♥ ":
-      column = 5
-      break;
-       case "Født":
-      column = 6
-      break;
-    case "Skip/Planet":
-      column = 7
-      break;
-  
-  }
-  
-  sortTable(column)
+
+// Kopier de fire linjene under her hvis du skal ha en tabell til (fra 'let personellTable')
+// På den dupliserte bytter du alle steder det står personellTable med f.eks spacecraftTable
+// (gitt at du har en tabell med klassenavn spacecraftTable)
+
+let personellTable = document.querySelector('.personellTable')
+personellTable.querySelector('thead').addEventListener('click', ev => {
+  getClickedHeaderAndSort(personellTable, ev.target);
 })
 
+// dette er en nye funksjon som tar seg av det som den switch / case saken gjorde tidligere
+function getClickedHeaderAndSort(table, target) {
+  let column = 0;
+  let columns = table.querySelectorAll('th');
+  for (let i = 0; i < columns.length; i++) {
+    if (columns[i] === target) {
+      column = i;
+      break
+    }
+  }
+  sortTable(column)
+}
+
+// Selve sorteringen av tabellen
 function sortTable(column) {
   let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
   table = document.querySelector("table");
@@ -69,4 +59,3 @@ function sortTable(column) {
     }
   }
 }
-})
